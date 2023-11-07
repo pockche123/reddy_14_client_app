9726de9c4e75624b626475df9f7ba50f0f8d37af
pockche123 <130081044+pockche123@users.noreply.github.com>
2023-11-07 11:43:46 +0000

    nav create

--- static/js/index.js
+++ static/js/index.js
@@ -25,12 +25,16 @@ document.getElementById('play-button').addEventListener('click', function () {
             let newButton = document.createElement('button');
             if (i === 0) {
                 newButton.className = 'btn btn-success difficulty-card';
+                newButton.id = 'easy'
             } else if (i === 1) {
                 newButton.className = 'btn btn-warning difficulty-card';
+                newButton.id = 'medium'
             } else {
                 newButton.className = 'btn btn-danger difficulty-card';
+                newButton.id = 'hard'
 
             }
+            newButton.addEventListener('click',() => navToQuiz(newButton.id))
             newButton.innerHTML = `
                     <label>${labels[i]}</label>
                 `;
@@ -42,3 +46,12 @@ document.getElementById('play-button').addEventListener('click', function () {
     }
     buttonsVisible = !buttonsVisible;
 });
+
+
+const easy = document.getElementsByClassName("difficulty-card")
+
+
+function navToQuiz(id) {
+    console.log("inside button")
+    window.location.href = `quiz.html?param=${id}`;
+}
